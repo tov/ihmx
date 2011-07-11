909dbb643a06092f962312d80d532f0e800b2698
Jesse A. Tov <jesse.tov@gmail.com>
2011-07-11 18:11:58 -0400

    got rid of Eq2 class

--- Eq1.hs
+++ Eq1.hs
@@ -4,7 +4,6 @@
 {- Equality type classes for unary and binary type constructors. -}
 module Eq1 (
   Eq1(..), EQ1(..),
-  Eq2(..), EQ2(..),
 ) where
 
 import Data.IORef
@@ -27,19 +26,4 @@ instance Eq1 TVar where eq1 = (==)
 newtype EQ1 t a = EQ1 (t a)
 instance Eq1 t ⇒ Eq1 (EQ1 t) where EQ1 x `eq1` EQ1 y = x `eq1` y
 instance Eq1 t ⇒ Eq (EQ1 t a) where EQ1 x == EQ1 y = x `eq1` y
-
--- | Like 'Eq1', but for binary type constructors.
-class Eq2 t where
-  eq2 ∷ t a b → t a b → Bool
-  ne2 ∷ t a b → t a b → Bool
-  x `ne2` y = not (x `eq2` y)
-
-infix 4 `eq2`, `ne2`
-
-instance Eq2 STRef where eq2 = (==)
-
--- | Injection for using 'Eq': If @t@ is 'Eq2' then @EQ2 t a b@ is 'Eq'
-newtype EQ2 t a b = EQ2 (t a b)
-instance Eq2 t ⇒ Eq2 (EQ2 t) where EQ2 x `eq2` EQ2 y = x `eq2` y
-instance Eq2 t ⇒ Eq (EQ2 t a b) where EQ2 x == EQ2 y = x `eq2` y
 
