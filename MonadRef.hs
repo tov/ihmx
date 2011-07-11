b61f1adecad0aea6a28a5cdccf84ccf3e0cc1e15
Jesse A. Tov <jesse.tov@gmail.com>
2011-07-11 18:11:11 -0400

    got rid of RefRef and RefT reference monad transformer

--- MonadRef.hs
+++ MonadRef.hs
@@ -11,11 +11,8 @@
   #-}
 module MonadRef (
   MonadRef(..),
-  RefT, RefRef, runRefT, mapRefT,
   UnsafeReadRef(..),
 ) where
-
-import Control.Applicative
 
 import Control.Monad.ST
 import Control.Monad.STM
@@ -53,80 +50,6 @@ class (UnsafeReadRef p, Monad m, Eq1 p) ⇒ MonadRef p m | m → p where
 
 class UnsafeReadRef p where
   unsafeReadRef ∷ p a → a
-
----
---- A transformer version of ST
----
-
-newtype RefT s m a = RefT { unRefT ∷ m a }
-  deriving Monad
-
-runRefT ∷ Monad m ⇒ (forall s. RefT s m a) → m a
-runRefT m = unRefT m
-
-mapRefT   ∷ (m a → m b) → RefT s m a → RefT s m b
-mapRefT f = RefT . f . unRefT
-
-liftCallCC :: (((a → m b) → m a) → m a) →
-              ((a → RefT s m b) → RefT s m a) → RefT s m a
-liftCallCC callCC f =
-  RefT $ callCC $ \ c -> unRefT (f (RefT . c))
-
-liftCatch ∷ (m a → (e → m a) → m a) →
-            RefT s m a → (e → RefT s m a) → RefT s m a
-liftCatch f m h = RefT $ f (unRefT m) (unRefT . h)
-
-instance Monad m ⇒ Functor (RefT s m) where
-  fmap  = liftM
-
-instance Monad m ⇒ Applicative (RefT s m) where
-  pure  = return
-  (<*>) = ap
-
-instance MonadTrans (RefT s) where
-  lift = RefT
-
-newtype RefRef s a = RefRef { unRefRef ∷ IORef (Box a) }
-  deriving Eq
-
-instance Eq1 (RefRef s) where eq1 = (==)
-instance Eq2 RefRef where eq2 = (==)
-
-data Box a = Box { unBox ∷ a }
-
-instance Monad m ⇒ MonadRef (RefRef s) (RefT s m) where
-  newRef a = return (RefRef (unsafePerformIO (newIORef (Box a))))
-  readRef (RefRef r) =
-    let a = unsafePerformIO (readIORef r)
-     in a `seq` return (unBox a)
-  writeRef (RefRef r) a =
-    unsafePerformIO (writeRef r (Box a)) `seq` return ()
-
-instance UnsafeReadRef (RefRef s) where
-  unsafeReadRef = unBox . unsafePerformIO . readIORef . unRefRef
-
-instance MonadCont m ⇒ MonadCont (RefT s m) where
-  callCC = liftCallCC callCC
-
-instance MonadError e m ⇒ MonadError e (RefT s m) where
-  throwError = lift . throwError
-  catchError = liftCatch catchError
-
-instance MonadReader r m ⇒ MonadReader r (RefT s m) where
-  ask     = lift ask
-  local   = mapRefT . local
-
-instance MonadState st m ⇒ MonadState st (RefT s m) where
-  get     = lift get
-  put     = lift . put
-
-instance MonadWriter w m ⇒ MonadWriter w (RefT s m) where
-  tell    = lift . tell
-  listen  = mapRefT listen
-  pass    = mapRefT pass
-
-instance MonadIO m ⇒ MonadIO (RefT s m) where
-  liftIO  = lift . liftIO
 
 ---
 --- Other MonadRef instances
